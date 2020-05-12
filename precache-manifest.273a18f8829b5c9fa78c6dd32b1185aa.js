7f2c4af984b211d96239ad501657a49f4bca4c05
Travis CI User <travis@example.org>
2020-05-12 03:52:48 +0000

    deploy

---
+++ precache-manifest.273a18f8829b5c9fa78c6dd32b1185aa.js
@@ -0,0 +1,250 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4891e506658593b23b1417654741c63b",
+    "url": "/react-antd-admin-template/index.html"
+  },
+  {
+    "revision": "397ac3c324fcc25fd85b",
+    "url": "/react-antd-admin-template/static/css/0.456e852a.chunk.css"
+  },
+  {
+    "revision": "64380a64b485be22262e",
+    "url": "/react-antd-admin-template/static/css/1.e995fb47.chunk.css"
+  },
+  {
+    "revision": "4ec5571ba30f0c49aee3",
+    "url": "/react-antd-admin-template/static/css/29.e7460dd0.chunk.css"
+  },
+  {
+    "revision": "55f95fa9d03589aa12e7",
+    "url": "/react-antd-admin-template/static/css/3.c999ae94.chunk.css"
+  },
+  {
+    "revision": "fc9c4243e1ba53d1ae41",
+    "url": "/react-antd-admin-template/static/css/30.6cb391d4.chunk.css"
+  },
+  {
+    "revision": "4f52d4ff7328a257e580",
+    "url": "/react-antd-admin-template/static/css/31.49b4d011.chunk.css"
+  },
+  {
+    "revision": "43fe0e09a8d7eb23761f",
+    "url": "/react-antd-admin-template/static/css/32.87d5c80c.chunk.css"
+  },
+  {
+    "revision": "9380b1c85879ed95be8b",
+    "url": "/react-antd-admin-template/static/css/35.6aa0c9e0.chunk.css"
+  },
+  {
+    "revision": "96040a550ab7fb9f61a2",
+    "url": "/react-antd-admin-template/static/css/5.915e11f9.chunk.css"
+  },
+  {
+    "revision": "1cefaa252049d63adbbe",
+    "url": "/react-antd-admin-template/static/css/Dashboard.b206ba95.chunk.css"
+  },
+  {
+    "revision": "7439a2e85907907ed2d9",
+    "url": "/react-antd-admin-template/static/css/Error404.b27f81ed.chunk.css"
+  },
+  {
+    "revision": "88eb5d8a2b012543be3c",
+    "url": "/react-antd-admin-template/static/css/RichTextEditor.bae160b2.chunk.css"
+  },
+  {
+    "revision": "54bb0fc3711aef40d626",
+    "url": "/react-antd-admin-template/static/css/UploadExcel.9579c327.chunk.css"
+  },
+  {
+    "revision": "ecb812bc135b019b6f8d",
+    "url": "/react-antd-admin-template/static/css/main.2324e87c.chunk.css"
+  },
+  {
+    "revision": "397ac3c324fcc25fd85b",
+    "url": "/react-antd-admin-template/static/js/0.2580a00c.chunk.js"
+  },
+  {
+    "revision": "64380a64b485be22262e",
+    "url": "/react-antd-admin-template/static/js/1.abc82561.chunk.js"
+  },
+  {
+    "revision": "42212f97415ff8fdfd37",
+    "url": "/react-antd-admin-template/static/js/2.5e988c4c.chunk.js"
+  },
+  {
+    "revision": "4ec5571ba30f0c49aee3",
+    "url": "/react-antd-admin-template/static/js/29.b0b6007d.chunk.js"
+  },
+  {
+    "revision": "80fc0a1eddff40de1f2574e818512c46",
+    "url": "/react-antd-admin-template/static/js/29.b0b6007d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55f95fa9d03589aa12e7",
+    "url": "/react-antd-admin-template/static/js/3.b1b3c91e.chunk.js"
+  },
+  {
+    "revision": "fc9c4243e1ba53d1ae41",
+    "url": "/react-antd-admin-template/static/js/30.fccc6002.chunk.js"
+  },
+  {
+    "revision": "3c548a488e94eaed40436091d8f7177d",
+    "url": "/react-antd-admin-template/static/js/30.fccc6002.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f52d4ff7328a257e580",
+    "url": "/react-antd-admin-template/static/js/31.6ccfa68d.chunk.js"
+  },
+  {
+    "revision": "43fe0e09a8d7eb23761f",
+    "url": "/react-antd-admin-template/static/js/32.bab7de8b.chunk.js"
+  },
+  {
+    "revision": "f18a7a6cbc750bfb3ed1acbe25c12343",
+    "url": "/react-antd-admin-template/static/js/32.bab7de8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4affcb81afd4e70674",
+    "url": "/react-antd-admin-template/static/js/33.57ec8ab3.chunk.js"
+  },
+  {
+    "revision": "31044c115fbf6d89e74d",
+    "url": "/react-antd-admin-template/static/js/34.512e4d98.chunk.js"
+  },
+  {
+    "revision": "9380b1c85879ed95be8b",
+    "url": "/react-antd-admin-template/static/js/35.2748584f.chunk.js"
+  },
+  {
+    "revision": "af87895fd9fd07ed3d97",
+    "url": "/react-antd-admin-template/static/js/36.e7dcaf6d.chunk.js"
+  },
+  {
+    "revision": "7d6a350411b784dd34ab",
+    "url": "/react-antd-admin-template/static/js/4.4b2dc111.chunk.js"
+  },
+  {
+    "revision": "455f9f3ae849b1b7c9d5b5f2d351830a",
+    "url": "/react-antd-admin-template/static/js/4.4b2dc111.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96040a550ab7fb9f61a2",
+    "url": "/react-antd-admin-template/static/js/5.fa7df520.chunk.js"
+  },
+  {
+    "revision": "d2b84eafe59c346f5d86",
+    "url": "/react-antd-admin-template/static/js/6.c274fe19.chunk.js"
+  },
+  {
+    "revision": "2c283c62e29454b9fd62",
+    "url": "/react-antd-admin-template/static/js/About.bd14392c.chunk.js"
+  },
+  {
+    "revision": "8d0ad62d5ff7719ea6c4",
+    "url": "/react-antd-admin-template/static/js/AdminPage.122ec4f3.chunk.js"
+  },
+  {
+    "revision": "a416d275eb5c28c6c636",
+    "url": "/react-antd-admin-template/static/js/Clipboard.1f38fde5.chunk.js"
+  },
+  {
+    "revision": "1cefaa252049d63adbbe",
+    "url": "/react-antd-admin-template/static/js/Dashboard.5e8f0cdd.chunk.js"
+  },
+  {
+    "revision": "c278ab68591ab70c34b2",
+    "url": "/react-antd-admin-template/static/js/Doc.16af0bd8.chunk.js"
+  },
+  {
+    "revision": "a1a7f473905accb1d38f",
+    "url": "/react-antd-admin-template/static/js/Draggable.fe3b0149.chunk.js"
+  },
+  {
+    "revision": "7439a2e85907907ed2d9",
+    "url": "/react-antd-admin-template/static/js/Error404.1325a6ca.chunk.js"
+  },
+  {
+    "revision": "6deaa78957904251253a",
+    "url": "/react-antd-admin-template/static/js/ExportExcel.95c16953.chunk.js"
+  },
+  {
+    "revision": "a6aff65aa98b3750dfcf",
+    "url": "/react-antd-admin-template/static/js/GuestPage.58968dee.chunk.js"
+  },
+  {
+    "revision": "5eed6ba4692d9e0cba37",
+    "url": "/react-antd-admin-template/static/js/Guide.0fd6e669.chunk.js"
+  },
+  {
+    "revision": "b72801ec7475b1d5bf50",
+    "url": "/react-antd-admin-template/static/js/KeyboardChart.5866853f.chunk.js"
+  },
+  {
+    "revision": "1d831ea50dc1154f3ad7",
+    "url": "/react-antd-admin-template/static/js/LineChart.8a687a42.chunk.js"
+  },
+  {
+    "revision": "a88a1b73cd259286e5f6",
+    "url": "/react-antd-admin-template/static/js/Markdown.80403694.chunk.js"
+  },
+  {
+    "revision": "7a325e1ad5f726a49614",
+    "url": "/react-antd-admin-template/static/js/Menu1_1.4fc886a9.chunk.js"
+  },
+  {
+    "revision": "47d1a67417ab658b8c46",
+    "url": "/react-antd-admin-template/static/js/Menu1_2_1.47853205.chunk.js"
+  },
+  {
+    "revision": "6ab277a9e0437b9faf20",
+    "url": "/react-antd-admin-template/static/js/MixChart.65ef91d2.chunk.js"
+  },
+  {
+    "revision": "88eb5d8a2b012543be3c",
+    "url": "/react-antd-admin-template/static/js/RichTextEditor.42e5ecf3.chunk.js"
+  },
+  {
+    "revision": "007101c988a470afb975",
+    "url": "/react-antd-admin-template/static/js/Table.21ea18ac.chunk.js"
+  },
+  {
+    "revision": "54bb0fc3711aef40d626",
+    "url": "/react-antd-admin-template/static/js/UploadExcel.91fcb557.chunk.js"
+  },
+  {
+    "revision": "0531722b8f46913bcb67",
+    "url": "/react-antd-admin-template/static/js/Zip.9ca4df19.chunk.js"
+  },
+  {
+    "revision": "ecb812bc135b019b6f8d",
+    "url": "/react-antd-admin-template/static/js/main.8361e3a9.chunk.js"
+  },
+  {
+    "revision": "903896140d268aa22100",
+    "url": "/react-antd-admin-template/static/js/runtime-main.3374e638.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/react-antd-admin-template/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "2701e8c903c6ea62b30e36aae59060a1",
+    "url": "/react-antd-admin-template/static/media/bg.2701e8c9.jpg"
+  },
+  {
+    "revision": "e58eaef0ee605eb27eb5117c9d10f189",
+    "url": "/react-antd-admin-template/static/media/draggable.e58eaef0.gif"
+  },
+  {
+    "revision": "106db4decc04072aaf88bc3226178395",
+    "url": "/react-antd-admin-template/static/media/logo.106db4de.png"
+  },
+  {
+    "revision": "5a232d53fba12e27ea5b5c16e5b1acd4",
+    "url": "/react-antd-admin-template/static/media/reward.5a232d53.jpg"
+  },
+  {
+    "revision": "2a5b6308244222a5b5335da0ebb9265b",
+    "url": "/react-antd-admin-template/static/media/wechat.2a5b6308.jpg"
+  }
+]);
